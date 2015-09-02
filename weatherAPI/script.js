29afd4c9d507a7b81662c4544f0ec5c6b4060ba4
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-01 22:10:41 -0700

    created a wireframe app that will tell you the temperature where you are

--- weatherAPI/script.js
+++ weatherAPI/script.js
@@ -1,8 +1,30 @@
-//Api Key: 78924cb5c89d8f7916f39a9c73145bc9
+//Open Weather Api Key: 78924cb5c89d8f7916f39a9c73145bc9
 
 $(document).ready(function( ) {
-  $.getJSON("http://ipinfo.io/json", function( data ){
-    var location = data.loc;
-    console.log(location);
-  });
+
+  function setWeather(temperature){
+    $(".current-weather").text("It is currently " + temperature + " degrees fahrenheit where you are.")
+  }
+
+  function getWeather(location) {
+    var latitude = location[0];
+    var longitude = location[1];
+
+    var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=imperial";
+
+    $.get( url, function( data ){
+      var temperature = data.main.temp;
+      setWeather(temperature);
+    }, "json" );
+  }
+
+  function findLocation( ) {
+    $.get("http://ipinfo.io", function( data ){
+      var location = data.loc.split( "," );
+      getWeather(location);
+    }, "json");
+  }
+
+  findLocation();
+
 });
