9ddcf7ce4334c50783f4c4361d362d4a438e54d4
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-03 10:43:11 -0700

    successfully grabbing online information

--- twitchAPI/scripts/main.js
+++ twitchAPI/scripts/main.js
@@ -15,7 +15,16 @@ $( document ).ready( function() {
   function getStream(stream){
     $.getJSON( url + "/streams/" + stream + "?client_id=" + token + "&callback=?" 
       ).success(function( data ) {
-        console.log(data);
+        if(data.stream === null) {
+          console.log(data);
+          console.log(data._links.channel);
+          console.log(data._links.self);
+        } else {
+          console.log(data._links.channel);
+          console.log(data._links.self);
+          console.log(data.stream.game);
+          console.log(data.stream.preview.small);
+        }
       });
   }
 
