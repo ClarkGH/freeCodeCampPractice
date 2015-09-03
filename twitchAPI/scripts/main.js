383d872974eabb824b69caea4273da929584a92d
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-02 20:33:01 -0700

    Successfully loading in API data

--- twitchAPI/scripts/main.js
+++ twitchAPI/scripts/main.js
@@ -1,3 +1,23 @@
-$( document ).ready(function() {
+$( document ).ready( function() {
+  var token = key.clientID; //saved in another file
+  var url = "https://api.twitch.tv/kraken";
+  var streamers = [
+                    "demolisho", 
+                    "boogie2988", 
+                    "freecodecamp", 
+                    "storbeck", 
+                    "terakilobyte", 
+                    "habathcx",
+                    "RobotCaleb",
+                    "thomasballinger",
+                    "noobs2ninjas",
+                    "beohoff"]
+  function getStream(stream){
+    $.getJSON( url + "/streams/" + stream + "?client_id=" + token + "&callback=?" 
+      ).success(function( data ) {
+        console.log(data);
+      });
+  }
 
+  getStream( streamers[2] );
 });
