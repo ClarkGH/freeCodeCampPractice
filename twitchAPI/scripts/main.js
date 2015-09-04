92c8f942583c5f344f0956d53f2085d25a341564
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-04 10:05:59 -0700

    Added a 24/7 stream. -have a successful link to a channel now

--- twitchAPI/scripts/main.js
+++ twitchAPI/scripts/main.js
@@ -3,7 +3,8 @@ $( document ).ready( function() {
   var url = "https://api.twitch.tv/kraken";
   var streamers = [
                     "demolisho", 
-                    "boogie2988", 
+                    "boogie2988",
+                    "medrybw", 
                     "freecodecamp", 
                     "storbeck", 
                     "terakilobyte", 
@@ -18,17 +19,19 @@ $( document ).ready( function() {
     $.getJSON( url + "/streams/" + stream + "?client_id=" + token + "&callback=?" 
       ).success(function( data ) {
         if(data.stream === null) {
-          console.log(data);
-          console.log(data.stream) //this will be null since there is no streaming ongoing
+          console.log("http://twitch.tv." + stream);
           console.log(data._links.channel);
           console.log(data._links.self);
-          $('.isStreaming').html('<a href=' + data.stream.channel.status + '>Google</a>');
         } else {
           isStreaming = true;
-          console.log(data._links.channel);
-          console.log(data._links.self);
-          console.log(data.stream.game);
-          console.log(data.stream.preview.small);
+          console.log("http://twitch.tv." + stream);
+          // console.log(data._links.channel);
+          // console.log(data._links.self);
+          console.log(data.stream) //this will be null since there is no streaming ongoing
+          // console.log(data.stream.url)
+          // console.log(data.stream.game);
+          // console.log(data.stream.preview.small);
+          $('.isStreaming').html('<a href= http://twitch.tv/' + stream + '>Google</a>');
         }
 
       });
