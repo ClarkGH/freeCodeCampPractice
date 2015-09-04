e3bbcd3eb4a778dd4904a327b36491a2020cbff0
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-04 13:37:32 -0700

    issues with asynchronous things -cleaned some code

--- twitchAPI/scripts/main.js
+++ twitchAPI/scripts/main.js
@@ -13,33 +13,33 @@ $( document ).ready( function() {
                     "thomasballinger",
                     "noobs2ninjas",
                     "beohoff"]
-  function getStream(stream){
-    var isStreaming = false;
-
+  function getStreamInfo(stream, index){
     $.getJSON( url + "/streams/" + stream + "?client_id=" + token + "&callback=?" 
       ).success(function( data ) {
         if(data.stream === null) {
-          console.log("http://twitch.tv." + stream);
-          console.log(data._links.channel);
-          console.log(data._links.self);
-          $('.isNotStreaming').html('<a href= http://twitch.tv/' + stream + '>' + stream + ' is not currently online. </a>');
-
-        } else {
-          isStreaming = true;
-          console.log("http://twitch.tv." + stream);
+          // console.log("http://twitch.tv." + stream);
           // console.log(data._links.channel);
           // console.log(data._links.self);
-          console.log(data.stream) //this will be null since there is no streaming ongoing
-          // console.log(data.stream.url)
+          // '<a class="online" id="stream' + index + '" href= http://twitch.tv/' + stream + '>' + stream + ' is not currently online. </a>'
+
+        } else {
+          // console.log("http://twitch.tv." + stream);
+          // console.log(data._links.channel);
+          // console.log(data._links.self);
+          // console.log(data.stream)
+          // console.log(data.stream.url)x
           // console.log(data.stream.game);
           // console.log(data.stream.preview.small);
-          $('.isStreaming').html('<a href= http://twitch.tv/' + stream + '>' + data.stream.channel.display_name + ' is now playing : ' + data.stream.channel.game + '</a>');
+            // '<a class="online" id="stream' + index + '" href= http://twitch.tv/' + stream + '>' + data.stream.channel.display_name + ' is now playing : ' + data.stream.channel.game + '</a>'
         }
-
       });
   }
 
-  for (var i = 0; i < streamers.length; i++) {
-    getStream(streamers[i]);
+  function getStreams(){ 
+    for (var i = 0; i < streamers.length; i++) {
+      getStreamInfo(streamers[i], i);
+    }
   }
+
+  getStreams();
 });
