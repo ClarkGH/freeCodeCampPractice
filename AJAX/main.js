20de01d6380f30c7f68c0848fdf8cd237b861e85
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-08 11:17:52 -0700

    Some changes

--- AJAX/main.js
+++ AJAX/main.js
@@ -1,2 +1,10 @@
 // XMLHttpRequest (JSObject objParameters);
-var xhr = XMLHttpRequest();
+var xhr = new XMLHttpRequest();
+
+function reqListener() {
+  console.log(this.responseText);
+}
+
+xhr.addEventListener("load", reqListener);
+xhr.open("GET", "http://www.example.org/example.txt", true);
+xhr.send()
