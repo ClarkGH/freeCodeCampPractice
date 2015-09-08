cad3dd3ac65d3bd9804a5dc3b7be2f31d7ccca6e
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-08 12:11:32 -0700

    set up basic AJAX -need to create node server next

--- AJAX/main.js
+++ AJAX/main.js
@@ -1,10 +1,15 @@
 // XMLHttpRequest (JSObject objParameters);
+// need a server for this to run properly
 var xhr = new XMLHttpRequest();
 
-function reqListener() {
-  console.log(this.responseText);
-}
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4) {
+    document.getElementById('ajax').innerHTML = xhr.responseText;
+  }
+};
 
-xhr.addEventListener("load", reqListener);
-xhr.open("GET", "http://www.example.org/example.txt", true);
-xhr.send()
+xhr.open('GET', 'crabby_patty.html');
+function sendAJAX() {
+  xhr.send();
+  document.getElementById('load').style.display = "none";
+}
