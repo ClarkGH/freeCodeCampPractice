9279f3e03c874f4ced353262027b1be6782fba12
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-08 14:03:59 -0700

    statuuuuus

--- AJAX/main.js
+++ AJAX/main.js
@@ -3,8 +3,10 @@
 var xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4) {
+  if (xhr.readyState === 4 && xhr.status === 200) {
     document.getElementById('ajax').innerHTML = xhr.responseText;
+  } else {
+    alert(xhr.statusText);
   }
 };
 
