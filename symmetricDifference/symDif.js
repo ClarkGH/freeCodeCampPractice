df9b485cef5ba3917f4e033d675c2fc0b802f383
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-09 21:22:41 -0700

    boop

--- symmetricDifference/symDif.js
+++ symmetricDifference/symDif.js
@@ -9,15 +9,26 @@
 // }
 
 function sym(args) {
-    var args = [];
-    for( var i = 0; i < arguments.length; i++){
+    var args = [args, ];
+    var commonVal = [];
+    for( var i = 1; i < arguments.length; i++){
       args.push(arguments[i]);
     }
-    // var args = [].slice.call(arguments); is the same thing as the above;
+
+    // var args = [].slice.call(arguments); is the same thing as the above 4 lines;
+
+    function getCommonVal(arr1, arr2){
+      return arr1.filter(function(val, i){
+        return arr2.indexOf(val) === -1;
+      });
+    };
+
+    commonVal.push(getCommonVal(args[0],args[1]));
+
 
 
     // return arg.map(unique).reduce(function(previous, current) {
         // return complement(previous, current).concat(complement(current, previous)); });
 }
 
-sym([1, 2, 3], [5, 2, 1, 4]);
+sym([1, 2, 3],[5, 2, 1, 4]);
