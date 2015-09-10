2a3e988813a1418fe06e852d207f5d998c03d49a
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-09 21:32:19 -0700

    cleaned out comments

--- symmetricDifference/symDif.js
+++ symmetricDifference/symDif.js
@@ -1,20 +1,8 @@
-// function complement(A, B) {
-//     return A.filter(function(elem) { return B.indexOf(elem) == -1; });
-// }
-
-// function unique(arr) {
-//   return arr.filter(function(elem, pos) {
-//     return arr.indexOf(elem) == pos;
-//   });
-// }
-
 function sym(args) {
     var args = [args, ];
     for( var i = 1; i < arguments.length; i++){
       args.push(arguments[i]);
     }
-
-    // var args = [].slice.call(arguments); is the same thing as the above 4 lines;
 
     function getCommonVal(arr1, arr2){
       return arr1.filter(function(val, i){
@@ -31,11 +19,6 @@ function sym(args) {
     return args.map(getUniqueVal).reduce(function(arr1, arr2){
       return getCommonVal(arr1, arr2).concat(getCommonVal(arr2, arr1));
     });
-
-
-
-    // return arg.map(unique).reduce(function(previous, current) {
-        // return complement(previous, current).concat(complement(current, previous)); });
 }
 
 sym([1, 2, 3],[5, 2, 1, 4]);
