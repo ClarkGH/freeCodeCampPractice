22c36e574207dad1d5ea5ec173d724ec96b0a8b5
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-17 15:59:46 -0700

    base node server

--- nodePlay/main.js
+++ nodePlay/main.js
@@ -1,34 +1,8 @@
-var http = require('http')
-var url = require('url')
+var http = require('http');
 
-function parsetime (time) {
-  return {
-    hour: time.getHours(),
-    minute: time.getMinutes(),
-    second: time.getSeconds()
-  }
-}
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World\n');
+}).listen(8000);
 
-function unixtime (time) {
-  return { unixtime : time.getTime() }
-}
-
-var server = http.createServer(function (req, res) {
-  var parsedUrl = url.parse(req.url, true)
-  var time = new Date(parsedUrl.query.iso)
-  var result
-
-  if (/^\/api\/parsetime/.test(req.url))
-    result = parsetime(time)
-  else if (/^\/api\/unixtime/.test(req.url))
-    result = unixtime(time)
-
-  if (result) {
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify(result))
-  } else {
-    res.writeHead(404)
-    res.end()
-  }
-})
-server.listen(Number(process.argv[2]))
+console.log('Server running at http://localhost:8000');
