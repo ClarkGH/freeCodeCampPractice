3b2c15cf0a5155b357665ebbe3ec48489ad52be2
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-18 15:45:41 -0700

    all the node here

--- nodeProj/coreDebugger.js
+++ nodeProj/coreDebugger.js
@@ -1,3 +1,15 @@
 // The best debugger in node is console.log() since it doesn't break the flow
 // fast and informative, but need to know where to put it
 
+var http = require('http');
+debugger;
+
+http.createServer(function (req, res) {
+  res.WriteHead(200, {'Content-Type': 'text/plain'});
+  debugger;
+  res.end('Hello World\n');
+}).listen(3000);
+
+console.log('Server is running at http://localhost:3000');
+
+// nothing will change unless we run $node debug coreDebugger.js
