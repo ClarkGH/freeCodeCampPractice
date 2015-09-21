2257be15ca9776056f83c7ab60ff672bd0dbe7d3
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-21 12:22:43 -0700

    refactored the server to variables

--- nodeProj/blogExpress/app.js
+++ nodeProj/blogExpress/app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-
 var http = require('http');
 var path = require('path');
 
@@ -16,14 +15,20 @@ app.all('*', function(req, res){
   );
 })
 
-http
-  .createServer(app)
-  .listen(
-    app.get('port'), 
-    function (){
-      console.log(
-        'Express server is listening on port ' + 
-        app.get('port')
-      );
-    }
-  );
+var server = http.createServer(app);
+var boot = function () {
+  server.listen(app.get('port'), function(){
+    console.info('Express server is listening on port ' + app.get('port'));
+  });
+}
+var shutdown = function() {
+  server.close();
+}
+if (require.main === module) {
+  boot();
+} else {
+  console.info('Running app as a module')
+  exports.boot = boot;
+  exports.shutdown = shutdown;
+  exports.port = app.get('port');
+}
