c8174be06ef4a307cc9f60d9ef00d540b8bfddbe
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-21 12:13:53 -0700

    Added app.js and index.jade basics -tests will still fail

--- nodeProj/blogExpress/test/index.js
+++ nodeProj/blogExpress/test/index.js
@@ -11,7 +11,7 @@ describe('server', function(){
   describe('homepage', function(){
     it('should respond to GET', function(done){
       superagent
-        .get('http://localhost'+port)
+        .get('http://localhost' + port)
         .end(function(res){
           expect(res.status).to.equal(200);
           done()
