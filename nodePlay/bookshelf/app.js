cdc23da0bf3ef58ad9d73931fc8ef7e9bce9834d
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-23 10:47:26 -0700

    created models

--- nodePlay/bookshelf/app.js
+++ nodePlay/bookshelf/app.js
@@ -11,3 +11,47 @@ var knex = require('knex')({
 });
 
 var Bookshelf = require('bookshelf')(knex);
+
+// MODELS
+
+// User model
+var User = Bookshelf.Model.extend({
+  tableName: 'users'
+});
+
+// Post model
+var Post = Bookshelf.Model.extend({
+  tableName: 'posts'
+
+  hasTimestamps: true,
+
+  category: function () {
+    return this.belongsTo(Category, 'category_id');
+  },
+
+  tags: function () {
+    return this.belongsToMany(Tag);
+  },
+
+  author: function () {
+    return this.belongsTo(User);
+  }
+});
+
+// Category model
+var Category = Bookshelf.Model.extend({
+  tableName: 'categories',
+
+  posts: function () {
+    return this.belongsToMany(Post, 'category_id')
+  }
+});
+
+// Tag model
+var Tag = Bookshelf.Model.extend({
+  tableName: 'tags',
+
+  posts: function () {
+    return this.belongsToMany(Post);
+  }
+});
