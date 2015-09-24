d7d750ef7cdf744ce77d1d1cb0184ff6d3886cbd
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-23 20:47:54 -0700

    figuring out router

---
+++ nodePlay/todo_app/routes/todos.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/todos', function(req, res, next) {
+  new Todo()
+    .fetchAll()
+    .then(function (collection) {
+      var todos = [];
+
+      for (var i = 0; i < collection.length; i++) {
+        var temp = [];
+        temp[0] = collection.at(i).attributes.todo;
+        temp[1] = collection.at(i).attributes.id;
+        todos.push(temp);
+      }
+      res.json(todos);
+    });
+});
+
+module.exports = router;
--- nodePlay/todo_app/server/app.js
+++ nodePlay/todo_app/server/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('../routes/index');
+var todos = require('../routes/todos');
 
 var app = express();
 
@@ -22,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/todos', todos);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
