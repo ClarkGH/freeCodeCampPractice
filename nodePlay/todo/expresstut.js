a913bf04af597243fd1e4d773c20e81c54ef5482
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-24 19:28:45 -0700

    rendering specific jade file

--- nodePlay/todo/expresstut.js
+++ nodePlay/todo/expresstut.js
@@ -1,7 +1,12 @@
-var path = require('path')
-var express = require('express')
-var app = express()
+var express = require ('express');
+var app = express();
+var path = require('path');
 
-app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
+app.set('view engine', 'jade')
+app.set('views', path.join(__dirname, 'templates'))
+
+app.get('/home', function (req, res) {
+  res.render(process.argv[3], {date: new Date().toDateString()})
+})
 
 app.listen(process.argv[2])
