7c7da3be42b404327771a960d0ffbbb94f0ec874
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-09-24 20:44:24 -0700

    more done

--- nodePlay/expresstut/expresstut4.js
+++ nodePlay/expresstut/expresstut4.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var app = express();
-var bodyparser = require('body-parser');
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
 
-app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.post('/form', function (req, res) {
-  res.end(req.body.str.split('').reverse().join(''))
+  res.send(req.body.str.split('').reverse().join(''))
 })
 
 app.listen(process.argv[2])
