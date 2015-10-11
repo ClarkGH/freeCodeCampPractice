70e8c120476572a448234424c61855d5d6294d41
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-10-10 19:44:31 -0700

    sass tastes baaaaad -more rick and morty puuuuns

--- timer/Gulpfile.js
+++ timer/Gulpfile.js
@@ -1,6 +1,18 @@
-var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+var gulp       = require('gulp'),
+    gulp-util  = require('gulp-util'),
+    sourcemaps = require('gulp-sourcemaps'),
+    sass       = require('gulp-sass');
 
-gulp.task('default', function () {
-  // default coooode
-})
+gulp.task('default', ['watch']);
+
+gulp.task('build-css', function() {
+  return gulp.src('source/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+      .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(output.stylesheets));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(input.sass, ['build-css']);
+});
