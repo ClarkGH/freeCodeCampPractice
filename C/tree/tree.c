d79d337c0fa509df9ccf066b4701ad2a3b7c4c8b
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-10-14 23:29:24 -0700

    no more recursion

--- C/tree/tree.c
+++ C/tree/tree.c
@@ -1,22 +1,20 @@
-int jsw_find_r(struct jsw_node *root, int data)
-{
-    if (root == NULL)
-    {
-        return 0;
-    }
-    else if (root->data == data)
-    {
-        return 1;
-    }
-    else
-    {
-        int dir = root->data < data;
-
-        return jsw_find_r(root->link[dir], data);
-    }
-}
-
 int jsw_find(struct jsw_tree *tree, int data)
 {
-    return jsw_find_r(tree->root, data);
+    struct jsw_node *it = tree->root;
+
+    while (it != NULL)
+    {
+        if (it->data == data)
+        {
+            return 1;
+        }
+        else
+        {
+            int dir = it->data < data;
+
+            it = it->link[dir];
+        }
+    }
+
+    return 0;
 }
