9e57668fc9bd27e6b80a50427d6577449a3849f1
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-10-17 23:42:55 -0700

    beer

---
+++ Ruby/intro/beer.rb
@@ -0,0 +1,7 @@
+bottles = 99
+
+while bottles > 0
+  puts ("#{bottles} bottles of beer on the wall. Take one down, pass it on down.")
+  bottles -= 1
+  puts (bottles + " bottles of beer on the wall.")
+end
