e365051b06e6fd1f0d1655b188195178804b03d3
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-10-17 21:34:24 -0700

    minutes in a decade

--- Ruby/intro/num_in_year.rb
+++
@@ -1,6 +0,0 @@
-year  = 1
-weeks = year * 52.1429
-days  = weeks * 7
-hours = days * 24
-
-puts "There are #{hours} hours in a year"
---
+++ Ruby/intro/time_math.rb
@@ -0,0 +1,7 @@
+years   = 10
+weeks   = 52
+days    = 7
+hours   = 24
+minutes = 60
+
+puts years * weeks * days * hours * minutes
