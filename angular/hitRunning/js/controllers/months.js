4e0f03651e4dfb3460292f3886fc747fdadc0733
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-01-03 15:11:38 -0800

    Working with filters

--- angular/hitRunning/js/controllers/months.js
+++ angular/hitRunning/js/controllers/months.js
@@ -1,9 +1,5 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('controller1', ['$scope', function ( $scope ) {
-  $scope.bacon = "bacon";
-}]);
-
-myApp.controller('controller2', ['$scope', function ( $scope ) {
-  $scope.ham = "ham";
+  $scope.price = 13.01;
 }]);
