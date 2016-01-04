653a4e6ce90bf07f50b14913dbb478d43ebe7e2a
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-01-03 16:05:32 -0800

    Why does the tutorial go over something already done in an assignment

--- angular/hitRunning/js/controllers/months.js
+++ angular/hitRunning/js/controllers/months.js
@@ -2,4 +2,24 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('controller1', ['$scope', function ( $scope ) {
   $scope.price = 13.01;
+
+  $scope.friends = [
+    {
+      name: 'Bilbo',
+      phone: '123-1224'
+    },
+    {
+      name: 'Jerry',
+      phone: '213-2321'
+    },
+    {
+      name: 'Bilbo',
+      phone: '213-1332'
+    },
+    {
+      name: 'Bilbo',
+      phone: '321-3312'
+    }
+  ];
+  
 }]);
